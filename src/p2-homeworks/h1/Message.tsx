fd1bbff90d8a66861bb27563801d8e194e5e385a
Pharmman <pharm.sale777@gmail.com>
2020-09-18 13:09:12 +0300

    Task01

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import {MessageDataType} from "./HW1";
+import s from "./Message.module.css"
 
-function Message() {
+function Message(props: MessageDataType) {
     return (
         <div>
-
+            <div className={s.message}>
+                <div className={s.avatar}>
+                    <img src={props.avatar} alt=""/>
+                </div>
+                <div className={s.message_item}>
+                    <p className={s.name}>{props.name}</p>
+                    <p className={s.text}>{props.message}</p>
+                    <div className={s.time}>{props.time}</div>
+                </div>
+            </div>
         </div>
     );
 }
