9778bc3ac694890d7ab2454707d6175acd72a0f1
Pharmman <pharm.sale777@gmail.com>
2020-09-27 20:56:35 +0300

    did_task_2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Affair from "./Affair";
 import {AffairType} from "./HW2";
 
-type AffairsPropsType = { // need to fix any
+type AffairsPropsType = {
     data: Array<AffairType>
     setFilter: (filter:string) => void
     deleteAffairCallback: (id:number) => void
@@ -10,7 +10,7 @@ type AffairsPropsType = { // need to fix any
 
 function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
+        <Affair
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
