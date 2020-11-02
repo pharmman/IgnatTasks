6dc4e0fa3ba622b1cbec69acec53c8feca259f98
Pharmman <pharm.sale777@gmail.com>
2020-11-02 12:16:43 +0300

    Made task 5

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './App.module.css';
-import HW5 from '../../../p2-homeworks/h5/HW5';
+import HW6 from '../../../p2-homeworks/h6/HW6';
 
 function App() {
     return (
@@ -10,8 +10,8 @@ function App() {
             <HW2/>*/}
             {/*<HW3/>*/}
             {/*<HW4/>*/}
-            <HW5/>
-
+            {/*<HW5/>*/}
+                <HW6/>
         </div>
     );
 }
--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,5 +1,6 @@
-import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from "react";
-import SuperInputText from "../../../h4/common/c1-SuperInputText/SuperInputText";
+import React, {DetailedHTMLProps, HTMLAttributes, InputHTMLAttributes, useState} from 'react';
+import SuperInputText from '../../../h4/common/c1-SuperInputText/SuperInputText';
+import classes from './SuperEditableSpan.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
@@ -32,21 +33,23 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // setEditMode(); // выключить editMode при нажатии Enter
-
+        setEditMode(false)
         onEnter && onEnter();
     };
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // setEditMode(); // выключить editMode при нажатии за пределами инпута
+        setEditMode(false)
 
         onBlur && onBlur(e);
     };
     const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
         // setEditMode(); // включить editMode при двойном клике
+        setEditMode(true)
 
         onDoubleClick && onDoubleClick(e);
     };
 
-    const spanClassName = `${"сделать красивый стиль для спана"} ${className}`;
+    const spanClassName = `${classes.span} ${className}`;
 
     return (
         <>
